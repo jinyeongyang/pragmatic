20231701a619f543d281ea76c3ce4f5cdbf48aa9
jinyeongyang <yjy@weuscnc.com>
2021-02-02 11:34:24 +0900

    django course 30 commit

---
+++ profileapp/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    # on_delete cascade 유저가 사라질 때 프로필도 사라지는 설정
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    message = models.CharField(mex_length=100, null=True)
--- project1/settings.py
+++ project1/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp',
+    'profileapp',
+
 ]
 
 MIDDLEWARE = [
