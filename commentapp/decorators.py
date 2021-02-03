761cfba4ef500d82c6df43614d17318c7dc07fd3
jinyeongyang <yjy@weuscnc.com>
2021-02-03 13:44:16 +0900

    django course 39 commit

--- commentapp/decorators.py
+++ commentapp/decorators.py
@@ -7,7 +7,7 @@ from commentapp.models import Comment
 def comment_ownership_required(func):
     def decorated(request, *args, **kwargs):
         comment = Comment.objects.get(pk=kwargs['pk'])
-        if not article.writer == request.user:
+        if not comment.writer == request.user:
             return HttpResponseForbidden()
         return func(request, *args, *kwargs)
     return decorated
