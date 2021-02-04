99931a6fded13f0ccb43a2bc5f1180b43566c45f
jinyeongyang <yjy@weuscnc.com>
2021-02-04 14:07:45 +0900

    django course 42 commit

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from commentapp.views import CommentCreateView, CommentDeleteView
 
-app_name = 'commentapp'
+app_name = "commentapp"
 
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
--- project1/settings.py
+++ project1/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     'profileapp',
     'articleapp',
     'commentapp',
+    'projectapp',
+    'subscribeapp',
 
 
 ]
